7f395b24a6b9486c3bf72d48b3fdd200503c7668
Valetina-2608 <valusha2608@gmail.com>
2022-06-24 19:46:30 +0300

    Add Function change_currency

--- script.js
+++ script.js
@@ -1,6 +1,36 @@
+
 /* Script */
 
+let currency=document.getElementById('currency');
+currency.addEventListener('change',change_currency);
 
+/* Change currency*/
 
-			
+function change_currency(event){
+let products_price=document.querySelectorAll('.product_price');
+for (let elem of products_price){
+
+	if (event.target.value==='UAH'){
+		let price=elem.innerHTML;
+		/* find the number part of product_price.innerHTML */
+		let price1=price.substring(0,price.length-1);
+		/*convert currency from Euro to UAH*/
+		let price_UAH=+price1*37
+		elem.innerHTML=price_UAH + ' UAH';
+		console.log(elem.innerHTML);
+	} 
+	if (event.target.value==='Euro'){
+		let price_UAH=elem.innerHTML;
+		/* find the number part of element product_price.innerHTML */
+		let price2=price_UAH.substring(0,price_UAH.length-3);
+		/*convert currency from UAH to "Euro*/
+		let price_Euro=+price2/37;
+		elem.innerHTML=price_Euro + ' Є';
+		console.log(elem.innerHTML);
 		
+	}
+}
+};
+			
+
+
